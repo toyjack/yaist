1f6ed715eab4b80533684c843806b33d47a4c6bc
toyjack <toyjack@gmail.com>
2021-11-18 12:11:07 +0900

    updated: xml customize dialog's style

--- components/xmlCustomize.vue
+++ components/xmlCustomize.vue
@@ -1,13 +1,18 @@
 <template>
-  <div class="card">
-    <div class="card-content">
-      <p class="title">TEIブロックを編集する</p>
+  <div class="modal-card">
+    <header class="modal-card-head">
+      <p class="modal-card-title">XMLテンプレート</p>
+      <button type="button" class="delete" @click="$emit('close')" />
+    </header>
+    <section class="modal-card-body">
+      <!-- <p class="title">TEIブロックを編集する</p> -->
       <p>[[unicode]]は「u4E00」のようなUnicodeスカラ値です。</p>
       <p>[[IDS]]は「⿰口土」のような漢字のIDS情報です。</p>
       <p>[[character]]は漢字符号です。</p>
       <p>
         [[GlyphWikiPNG]]はGlyphWikiのPNG画像URLです。[[GlyphWikiSVG]]にすると、SVGファイルのURLになります。
       </p>
+      <br>
       <b-field label="TEIブロックのテンプレート">
         <b-input
           type="textarea"
@@ -18,7 +23,8 @@
       <div class="buttons is-right">
         <b-button label="保存" @click="$emit('close')"></b-button>
       </div>
-    </div>
+    </section>
+    <footer class="modal-card-foot"></footer>
   </div>
 </template>
 
--- pages/index.vue
+++ pages/index.vue
@@ -85,29 +85,19 @@
             @click="isShowXmlCustomize = !isShowXmlCustomize"
           />
         </div>
-        <b-modal v-model="isShowXmlCustomize" trap-focus aria-role="dialog">
-         <xml-customize :xml-template.sync="xmlBlock" @close="isShowXmlCustomize=false">
-         </xml-customize>
-          <!-- <div class="card">
-            <div class="card-content">
-              <p class="title">TEIブロックを編集する</p>
-              <p>[[unicode]]は「u4E00」のようなUnicodeスカラ値です。</p>
-              <p>[[IDS]]は「⿰口土」のような漢字のIDS情報です。</p>
-              <p>[[character]]は漢字符号です。</p>
-              <p>
-                [[GlyphWikiPNG]]はGlyphWikiのPNG画像URLです。[[GlyphWikiSVG]]にすると、SVGファイルのURLになります。
-              </p>
-              <b-field label="TEIブロックのテンプレート">
-                <b-input type="textarea" v-model="xmlBlock"></b-input>
-              </b-field>
-              <div class="buttons is-right">
-                <b-button
-                  label="保存"
-                  @click="isShowXmlCustomize = false"
-                ></b-button>
-              </div>
-            </div>
-          </div> -->
+        <b-modal
+          v-model="isShowXmlCustomize"
+          trap-focus
+          aria-role="dialog"
+          has-modal-card
+          aria-label="XMLテンプレート"
+          close-button-aria-label="閉じる"
+          aria-modal
+        >
+          <xml-customize
+            :xml-template.sync="xmlBlock"
+            @close="isShowXmlCustomize = false"
+          />
         </b-modal>
       </section>
     </div>
@@ -120,7 +110,8 @@
         :idsData="ids"
       />
     </div>
-  </div> <!-- end for div.columns -->
+  </div>
+  <!-- end for div.columns -->
 </template>
 
 <script>
