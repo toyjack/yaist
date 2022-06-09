e7ddf7c5f7664ee5342d7ebe8e264512da9254ab
toyjack <toyjack@gmail.com>
2022-06-09 19:32:28 +0900

    added footer

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,6 @@
 <template>
   <div>
-    <nav
-      class="navbar is-fixed-top header has-shadow is-primary"
-      role="navigation"
-      aria-label="main navigation"
-    >
+    <nav class="navbar is-fixed-top header has-shadow is-primary" role="navigation" aria-label="main navigation">
       <div class="navbar-brand">
         <!-- <a class="navbar-item" href="/"> HIIDS </a> -->
         <a class="navbar-item" href="/"> YAIST(Yet Another IDS Search Tool) </a>
@@ -21,15 +17,9 @@
             <a class="navbar-link"> Lang </a>
 
             <div class="navbar-dropdown">
-              <nuxt-link :to="switchLocalePath('zh-cn')"
-                ><a class="navbar-item"> 中文 </a></nuxt-link
-              >
-              <nuxt-link :to="switchLocalePath('ja')"
-                ><a class="navbar-item"> 日本語 </a></nuxt-link
-              >
-              <nuxt-link :to="switchLocalePath('en')"
-                ><a class="navbar-item"> English </a></nuxt-link
-              >
+              <nuxt-link :to="switchLocalePath('zh-cn')"><a class="navbar-item"> 中文 </a></nuxt-link>
+              <nuxt-link :to="switchLocalePath('ja')"><a class="navbar-item"> 日本語 </a></nuxt-link>
+              <nuxt-link :to="switchLocalePath('en')"><a class="navbar-item"> English </a></nuxt-link>
 
               <!-- <nuxt-link :to="switchLocalePath('zh-cn')"
                 ><a class="navbar-item"> Simplified Chinese </a></nuxt-link
@@ -41,15 +31,9 @@
           </div>
           <div class="navbar-item">
             <div class="buttons">
-              <b-button
-                :label="$t('label.changeTemplate')"
-                type="is-dark"
-                @click="isShowXmlCustomize = !isShowXmlCustomize"
-              />
-              <b-button
-                :label="$t('label.manual')"
-                @click="isManualShow = !isManualShow"
-              />
+              <b-button :label="$t('label.changeTemplate')" type="is-dark"
+                @click="isShowXmlCustomize = !isShowXmlCustomize" />
+              <b-button :label="$t('label.manual')" @click="isManualShow = !isManualShow" />
             </div>
           </div>
         </div>
@@ -59,15 +43,8 @@
     <b-modal v-model="isManualShow" :width="640" scroll="keep">
       <Manual @close="isManualShow = false" />
     </b-modal>
-    <b-modal
-      v-model="isShowXmlCustomize"
-      trap-focus
-      aria-role="dialog"
-      has-modal-card
-      :aria-label="$t('label.customTemplate')"
-      close-button-aria-label="X"
-      aria-modal
-    >
+    <b-modal v-model="isShowXmlCustomize" trap-focus aria-role="dialog" has-modal-card
+      :aria-label="$t('label.customTemplate')" close-button-aria-label="X" aria-modal>
       <xml-customize @close="isShowXmlCustomize = false" />
     </b-modal>
 
@@ -76,6 +53,17 @@
         <Nuxt />
       </div>
     </section>
+
+    <footer class="footer">
+      <div class="content ">
+        <p class="has-text-centered has-text-justified">
+          <strong>YAIST</strong> by <a href="https://researchmap.jp/liuguanwei">Guanwei Liu</a>. The source code is
+          licensed
+          <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. The website content
+          is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY NC SA 4.0</a>.
+        </p>
+      </div>
+    </footer>
   </div>
 </template>
 
